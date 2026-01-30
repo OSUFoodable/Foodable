2b9fb082e7d1bf1f2ee02461f934477038703ca5
Bryan Piedra <piedrabr@oregonstate.edu>
2026-01-29 23:22:54 -0800

    Work on Profile Page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext.jsx";
+
 export default function Profile() {
+  const { user } = useContext(AuthContext);
+  if (!user) return <p>Loading user information...</p>;
+  
   return (
     <div style={{ padding: "1.5rem" }}>
+      <h2>Welcome, {user["cognito:username"]}!</h2>
       <h1>Profile</h1>
       <p>Profile settings and dietary preferences. (placeholder)</p>
     </div>
