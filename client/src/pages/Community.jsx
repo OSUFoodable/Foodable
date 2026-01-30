a7fd5fffd144929dab9c3e55691ad5ac2682f660
Bryan Piedra <piedrabr@oregonstate.edu>
2026-01-29 23:32:44 -0800

    Work in progress on Community Page

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext.jsx";
+
 export default function Community() {
+  const { user, logout } = useContext(AuthContext);
+  if (!user) return <p>Loading user information...</p>;
+
   return (
     <div style={{ padding: "1.5rem" }}>
+      <h2>Welcome, {user["cognito:username"]}!</h2>
       <h1>Community</h1>
       <p>See posts and activity from other Foodable users. (placeholder)</p>
     </div>
